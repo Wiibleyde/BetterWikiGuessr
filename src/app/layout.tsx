658ae1e0fa7e23cd89b1820069d277f305535fbc
Wiibleyde <nathan@bonnell.fr>
2026-02-14 18:03:49 +0100

    feat: initialize Wiki Guessr app with layout, global styles, and homepage
    - Added globals.css for Tailwind CSS integration.
    - Created layout.tsx for the root layout with metadata.
    - Implemented a simple homepage in page.tsx with a title.
    - Configured TypeScript with tsconfig.json for Next.js support.
    
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "Wiki Guessr",
+    description:
+        "Un jeu où vous devez deviner la page Wikipédia en donnant des mots !",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="fr">
+            <body className={`antialiased m-0 p-0`}>{children}</body>
+        </html>
+    );
+}
