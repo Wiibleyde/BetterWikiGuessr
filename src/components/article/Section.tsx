7a66c4be47900e2e8f06d2e273d4754f61844938
Nathan Bonnell <nathan@bonnell.fr>
2026-03-10 20:38:38 +0100

    fix: Various changes (#28)
    * feat: enhance game state management and UI components for guessing functionality
    
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>
    
    * feat: refactor components and hooks for improved structure and functionality
    
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>
    
    ---------
    
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- src/components/article/Section.tsx
+++ src/components/article/Section.tsx
@@ -1,5 +1,5 @@
+import TokenList from "@/components/TokenList";
 import type { RevealedMap, Token } from "@/types/game";
-import TokenList from "../TokenList";
 
 interface SectionProps {
     sectionIndex: number;
@@ -8,22 +8,24 @@ interface SectionProps {
     revealed: RevealedMap;
 }
 
-const Section = ({
+export function Section({
     sectionIndex,
     titleTokens,
     contentTokens,
     revealed,
-}: SectionProps) => {
+}: SectionProps) {
     return (
         <section className="p-5 bg-white rounded-xl shadow-sm border border-gray-100">
-            <h3 className="text-lg font-semibold mb-2 leading-[2.2]">
-                <TokenList
-                    tokens={titleTokens}
-                    section={sectionIndex}
-                    part="title"
-                    revealed={revealed}
-                />
-            </h3>
+            {titleTokens.length > 0 && (
+                <h3 className="text-lg font-semibold mb-2 leading-[2.2]">
+                    <TokenList
+                        tokens={titleTokens}
+                        section={sectionIndex}
+                        part="title"
+                        revealed={revealed}
+                    />
+                </h3>
+            )}
             <div className="text-sm leading-[2.2]">
                 <TokenList
                     tokens={contentTokens}
@@ -34,6 +36,6 @@ const Section = ({
             </div>
         </section>
     );
-};
+}
 
 export default Section;
